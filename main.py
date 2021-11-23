6d4c0cd84e7a22eb2b0fa93ee3e09ac050a6115a
Alex <alexander.mcfadden@gmail.com>
2021-11-22 16:23:47 -0800

    add/remove files

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+
+client = discord.Client()
+
+names = {}
+newline = '\n'
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$add'):
+        names[message.author] = None
+        await message.channel.send(f"Added {message.author.name}. Current list: {newline.join(i.name for i in names.keys())}")
+
+    if message.content.startswith('$remove'):
+        names.pop(message.author)
+        await message.channel.send(f"Removed {message.author.name}. \n Current list: \n {newline.join(i.name for i in names.keys())}")
+
+client.run("OTExNzIxMTEwMDIwNzEwNDYw.YZlgcw.hyAfdoQx5BfcXHyrTl8-KW9bm5o")
